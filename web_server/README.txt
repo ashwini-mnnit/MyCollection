8ca0ce586b794d9d9da765f5b2724e247cf56066
ashwini-mnnit <ashwini.mnnit@gmail.com>
2014-01-06 01:23:45 -0600

    Re organizing

---
+++ web_server/README.txt
@@ -0,0 +1,8 @@
+This is a minimalistic web server that serves you a local index file.
+
+Run your server as follows -
+
+$ gcc -o tserver -pthread threaded_server.c 
+$ ./tserver <port number> 
+
+In your browser, go to http://localhost:[port number]
---
+++ web_server/src/Respnse.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sstream>
+using namespace std;
+#define BUFSIZ 4096
+string getResponse(string filename )
+{
+   int port = 80;
+   int conn;
+   string recvStr = "";
+   const char *method = "GET";
+   const char *version = "1.1";
+   string url;
+   static struct sockaddr_in to_addr;
+   ostringstream  msg;
+   string host = "192.168.1.100";
+   if ((conn = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        cout<< __FUNCTION__ <<"::Can not create socket."<< endl;
+        return recvStr;
+   }
+   to_addr.sin_family = AF_INET;
+   inet_pton(AF_INET, (const char*)host.c_str(), &(to_addr.sin_addr));
+   to_addr.sin_port = htons(port);
+   if (connect(conn, (struct sockaddr *) &to_addr, sizeof(struct sockaddr_in)) < 0)
+   {
+       cout << __FUNCTION__ <<"::Can not connect to original server"<< endl;
+       close(conn);
+       return recvStr;
+   }
+   cout<<"sending req"<<endl;
+   char buf[BUFSIZ];
+  cout<<msg.str()<<endl;
+   msg << method << " " <<"/"<< filename << " " << "HTTP/" << version << "\r\nHost: " << host << "\r\n\r\n";
+  cout<<msg.str()<<endl;
+   int  sendBytes = send(conn, msg.str().c_str(), msg.str().length(), 0);
+   if (sendBytes < 0) {
+       close(conn);
+       cout<<__FUNCTION__<<"::not able to send request."<<endl;
+       return recvStr;
+   } else if (sendBytes != (int)msg.str().length()) {
+       close(conn);
+       cout<<__FUNCTION__<<"::not able to send request."<<endl;
+       return recvStr;
+   }
+   int len = 0;
+   while((len = recv( conn, buf, sizeof(buf), 0)) > 0){
+        buf[len]='\0';
+        recvStr.append(buf,len);
+   }
+   close(conn);
+   return recvStr;
+}
+
+int main()
+{
+   string filename = "IMG_0186.JPG";
+   string rv = getResponse(filename);
+   cout<<rv;
+   return 0;
+}
